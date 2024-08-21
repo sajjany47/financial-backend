91cb739f4280569548d0f5954ca6c51976a434e4
QbAnurag <anurag.patra@qbent.in>
2024-08-21 18:00:34 +0530

    changes

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,38 +7,42 @@ import { generateAccessToken, generateRefreshToken } from "../utilis/utilis.js";
 export const tokenValidation = async (req, res, next) => {
   const authToken = req.header("authorization");
 
-  if (!authToken || !authToken.startsWith("Bearer ")) {
+  if (typeof authToken !== "undefined") {
+    const token = authToken.split(" ")[1];
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, result) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ message: "Invalid token. User does not exist" });
+      }
+
+      const verifySession = employee.findOne({
+        _id: new mongoose.Types.ObjectId(result._id),
+      });
+      console.log(verifySession);
+      console.log("d", verifySession.sessionId);
+      console.log("l", result.sessionId);
+      if (!verifySession) {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ message: "Invalid session. User login first" });
+      }
+
+      if (verifySession.sessionId !== result.sessionId) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          message: "Access Denied due to new login from another device.",
+        });
+      }
+
+      req.user = result;
+
+      next();
+    });
+  } else {
     return res
       .status(StatusCodes.UNAUTHORIZED)
       .json({ message: "Access Denied. No token provided." });
-  }
-
-  try {
-    const token = authToken.split(" ")[1];
-
-    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-
-    const verifySession = await employee.findOne({
-      _id: new mongoose.Types.ObjectId(verifyToken._id),
-    });
-
-    if (!verifySession) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ message: "Invalid token. User does not exist." });
-    }
-
-    if (verifySession.sessionId !== verifyToken.sessionId) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        message: "Access Denied due to new login from another device.",
-      });
-    }
-
-    req.user = verifyToken;
-
-    next();
-  } catch (error) {
-    res.status(StatusCodes.UNAUTHORIZED).json({ message: error });
   }
 };
 
@@ -54,6 +58,7 @@ export const refreshTokens = async (req, res, next) => {
       .header("Authorization", `Bearer ${accessToken}`)
       .json({ accessToken: accessToken, refreshToken: refreshToken });
   } catch (error) {
+    console.log("object");
     return res.status(StatusCodes.BAD_REQUEST).json("Invalid refresh token.");
   }
 };
