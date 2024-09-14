cfb5ee1cd83d8aca43166da3b76c167e3b5e9ea9
sajjany47 <sajjany47@gmail.com>
2024-09-14 21:03:54 +0530

    loan model create

---
+++ src/api/loan/loan.config.js
@@ -0,0 +1,14 @@
+export const EmployeeTypes = ["salaried", "self_employed", "business"];
+
+export const LoanApplicationSteps = [
+  "application_number_generated",
+  "loan_amount_generated",
+  "document_address_verification",
+  "business_address_verification",
+  "document_verification",
+  "loan_amount_approved_processing",
+  "loan_approved",
+  "rejected",
+];
+
+export const ResidenceTypes = ["rented", "owned", "parent"];
